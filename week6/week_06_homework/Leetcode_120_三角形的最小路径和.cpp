85a10def856a916435684a8c7c8506f2e6db4c59
litongxue <13191062144@163.com>
2021-07-28 19:40:32 +0800

    week03 commit

---
+++ week6/week_06_homework/Leetcode_120_三角形的最小路径和.cpp
@@ -0,0 +1,19 @@
+﻿/*
+    由于从上往下枚举需要考虑边界问题，采用从下往上枚举的方式;
+                    集合：f[i][j] 从底向上走到(i,j)的路径和
+            状态表示
+                    属性：min
+    动态规划
+            状态计算：f[i][j] = min(f[i+1][j] , f[i+1][j+1]) + nums[i][j];
+*/
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& f) {
+        for (int i = f.size() - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                f[i][j] += min(f[i + 1][j], f[i + 1][j + 1]);
+            }
+        }
+        return f[0][0];
+    }
+};
---
+++ week6/week_06_homework/Leetcode_673_最长递增子序列的个数.cpp
@@ -0,0 +1,26 @@
+﻿/*
+    f[i] : 所有以nums[i]为结尾的递增子序列的长度;
+    g[i] : 以nums[i]为结尾的最长上升子序列的个数;
+    f[i] = max(f[i], f[j] + 1)  j<i, aj<ai;
+    (1) f[i] < f[j]+1  g[i] = g[j];
+    (2) f[i] == f[j]+1 g[i] += g[j];
+*/
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> f(n, 1), g(n, 1);
+        int maxl = 0, cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    if (f[i] < f[j] + 1) f[i] = f[j] + 1, g[i] = g[j];
+                    else if (f[i] == f[j] + 1) g[i] += g[j];
+                }
+            }
+            if (maxl < f[i]) maxl = f[i], cnt = g[i];
+            else if (maxl == f[i]) cnt += g[i];
+        }
+        return cnt;
+    }
+};
